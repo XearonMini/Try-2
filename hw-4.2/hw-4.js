c7e79ce85fa48d43f8b43bb1ae9c30e5ec4e728a
XearonMini <4632347@inbox.ru>
2023-01-26 19:57:33 +0300

    Deletion and delay added!

--- hw-4.2/hw-4.js
+++ hw-4.2/hw-4.js
@@ -11,5 +11,25 @@ fs.moveSync("Dir1/file.txt","Dir2/file.txt", err => {
 })
 fs.copy("Dir2/file.txt", "Dir3/file.txt", err =>{
     if (err) return console.error(err)
-    console.log("Copied 'file.txt' to 'Dir3' ")
+    console.log("Copied 'file.txt' to 'Dir3!' ")
 })
+setTimeout(function() { fs.emptyDir('Dir2', err => {
+    if (err) return console.error(err)
+    console.log('File in Dir2 deleted!')
+}) },100)
+setTimeout(function() { fs.emptyDir('Dir3', err => {
+    if (err) return console.error(err)
+    console.log('File in Dir3 deleted!')
+}) },100)
+setTimeout(function() { fs.remove('Dir1', err => {
+    if (err) return console.error(err)
+    console.log('Dir1 deletion success!')
+})},100)
+setTimeout(function() { fs.remove('Dir2', err => {
+    if (err) return console.error(err)
+    console.log('Dir2 deletion success!')
+})},100)
+setTimeout(function() { fs.remove('Dir3', err => {
+    if (err) return console.error(err)
+    console.log('Dir3 deletion success!')
+})},100)
