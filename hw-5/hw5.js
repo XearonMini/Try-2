6d90ebaa499cd7377f75fb0a5d3b602f9539f2ce
XearonMini <4632347@inbox.ru>
2023-01-31 16:29:29 +0300

    Done

---
+++ hw-5/hw5.js
@@ -0,0 +1,32 @@
+let num = 49;
+const str = 'string';
+var bool = true;
+console.log('First is +')
+console.log(str + num)
+console.log(num + bool)
+console.log(str + bool)
+console.log('Next is *')
+console.log(str * num)
+console.log(num * bool)
+console.log(str * bool)
+console.log('Next is /')
+console.log(str / num)
+console.log(num / bool)
+console.log(str / bool)
+console.log('Next is -')
+console.log(str - num)
+console.log(num - bool)
+console.log(str - bool)
+const x = '4'
+Number(x)
+console.log(x,typeof x)
+const y = 4
+String(y)
+console.log(y,typeof y)
+const z = b = n = true
+String(z)
+console.log(z,typeof z)
+b.toString();
+console.log(b,typeof b)
+Number(n)
+console.log(n,typeof n)
