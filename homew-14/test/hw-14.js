6b61b18244109094123fe1756266c0b677c91db1
XearonMini <4632347@inbox.ru>
2023-03-10 07:57:04 +0000

    Added several tests + function

--- homew-14/test/hw-14.js
+++ homew-14/test/hw-14.js
@@ -28,4 +28,13 @@ describe('Calculator tests', function() {
     it('shouldnt devide zero', async () => {
         expect(calc.divide(0, 1)).to.be.equal(0)
     })
+    it('int32 test', async () => {
+        expect(calc.add(9007199254740992, 1)).to.be.equal(9007199254740992) //int32
+    })
+    it('should sqrt number', async () => {
+        expect(calc.sqrt(36)).to.be.equal(6)  //had a fantasy leak so added some functions
+    })
+    it('should be able to round numbers test', async () => {
+        expect(Math.round(calc.sqrt(67)*100)/100).to.be.equal(8.19)//had a fantasy leak so added some functions
+    })
 });
